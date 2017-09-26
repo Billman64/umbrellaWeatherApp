4bdc3480f3699ba0e306ff99717c0b6796a4f8f8
Bill Lugo <lugow1@mail.montclair.edu>
2017-09-26 11:16:19 -0400

    Gathered and displaying hourly weather data in listView, using arrays to pass data through.

--- src/main/java/com/foo/umbrella/ui/getZipCode.java
+++ src/main/java/com/foo/umbrella/ui/getZipCode.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import com.foo.umbrella.R;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -50,6 +51,9 @@ public class getZipCode extends AppCompatActivity {
     public double wind;
     public String precipitation;
 
+    public String hourlyArr[][] = {{"a","b"}, {"a2","b2"} };
+    public int rMax=0;
+
     String strResult = "";
 
 
@@ -80,13 +84,10 @@ public class getZipCode extends AppCompatActivity {
 
 
         final Handler handler = new Handler(getApplicationContext().getMainLooper());
-//        String arr[] = new String[12];
-//        arr[0] = "asdf";
 
 
         // API call (TODO: refactor into data model area of code)
         String zip = et.getText().toString();
-//        final String strUrl = "http://api.wunderground.com/api/" + getString(R.string.api_key) + "/geolookup/q/" + zip + ".json";
         final String strUrl = "http://api.wunderground.com/api/" + getString(R.string.api_key) + "/conditions/hourly/q/" + zip + ".json";
         tv.setText(strUrl);
         Button btn = (Button) findViewById(R.id.button);
@@ -110,16 +111,13 @@ public class getZipCode extends AppCompatActivity {
                         }
                     });
 
+                    // Gather JSON data from API ----------------------
                     URL url = new URL(strUrl);
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();    // needs to run on a thread
-                    strResult = "error: before InputStreamReader";
-
-                    // Error - MainThread Exception somehow
 
                     InputStreamReader isr = new InputStreamReader(connection.getInputStream()) {
                         @Override
                         public int read() throws IOException {
-//                            Toast.makeText(MainActivity.this, "InputStreamReader error", Toast.LENGTH_SHORT).show();
                             Log.d("myError", "inputStreamReader error");
                             return 0;
                         }
@@ -127,10 +125,7 @@ public class getZipCode extends AppCompatActivity {
                     Log.d("connection", connection.toString());
 //                    Reader rdr = new InputStreamReader(isr);
 
-                    strResult = "error: before buffered reader";
-
-                    BufferedReader reader = new BufferedReader(isr); //???
-                    strResult = "error: after buffered reader";
+                    BufferedReader reader = new BufferedReader(isr);
 
                     StringBuffer json = new StringBuffer(1024); // use StringBuilder instead?
                     String tmp;
@@ -139,24 +134,24 @@ public class getZipCode extends AppCompatActivity {
                         json.append(tmp).append("\n");
                     }
                     reader.close();
-//                    Log.d("jsonData",json.toString() + " |tmp: " + tmp);
-                    strResult = "error: after readLine loop";
-
 
                     final JSONObject data = new JSONObject(json.toString());
                     strResult = "error: after JSONObject initialization";
 
                     if (data.length() == 0) {
-                        strResult = "error: zero-length data";
+                        strResult = "error: zero-length data"; Log.d("JSON data length","0");
                     } else {
-                        // parse JSON data
+
+                        // Parse JSON data -------------------------
                         final JSONObject current = new JSONObject(data.getString("current_observation"));
                         final JSONObject display_location = new JSONObject(current.getString("display_location"));
+                        final JSONArray hourly = new JSONArray(data.getString("hourly_forecast")); // error - need JSON array
+                        rMax = hourly.length();
 
                         Log.d("forecastDataSample",current.toString().substring(0,50));
-                        strResult = "error after data length>0";
                         Log.d("dataSample", data.toString().substring(0, 50));
 
+                        // Gather data points - current weather
                         if (current.toString().length()>0) {
                                 city = display_location.getString("city");
                                 state = display_location.getString("state");
@@ -165,17 +160,39 @@ public class getZipCode extends AppCompatActivity {
                                 humidity = current.getString("relative_humidity");
                                 wind = current.getDouble("wind_mph");
                                 precipitation = current.getString("precip_1hr_in");
-
                                 Log.d("cityState",city + "," + state);
-                            } else strResult = "error: zero-length data forecast";
+                            } else Log.d("Current weather", "data not found");
+
+                        // Gather data points - hourly weather
+                        strResult = "current JSON created. starting hourly JSON";
+                        Log.d("JSON gathering","current complete");
+
+                        if (hourly.length() >0) {
+                            Log.d("JSON gathering", "process started! array length: "+hourly.length());
+                            int r;
+                            JSONObject hourlyObj;
+                            hourlyArr = new String[hourly.length()][5];
+
+                            Log.d("JSON gathering", "hourlyObj created");
+                            // loop through hourly JSON data and gather it
+
+                            for(r=0;r<hourly.length();r++) {
+                                hourlyObj = new JSONObject(hourly.getString(r));
+//                                Log.d("JSON gathering", "hourlyObj initialized");
+//                                Log.d("JSON arry test", hourly.getString(0).substring(0, 600));
+                                hourlyArr[r][0] = hourlyObj.getString("condition");
+                                hourlyArr[r][1] = hourlyObj.getString("feelslike");
+                                hourlyArr[r][2] = hourlyObj.getString("humidity");
+                                hourlyArr[r][3] = hourlyObj.getString("wspd"); // sub-object
+                                hourlyArr[r][4] = hourlyObj.getString("snow") ; //
+                            }
+
+                            Log.d("hourly data sample",hourlyArr[0][0] +" "+ hourlyArr[0][1]);
+
+
+
+                            } else Log.d("Hourly weather", "data not found");
                         }
-
-
-                    // send JSON data back to main thread via a bundled message object
-//                    Message msgObj = handler.obtainMessage();
-//                    Bundle bundle = new Bundle();
-//                    bundle.putString("message", data.toString());
-//                    msgObj.setData(bundle);
 
                     handler.post(new Runnable() {
                         @Override
@@ -183,15 +200,13 @@ public class getZipCode extends AppCompatActivity {
                             // main thread communication
 
                             strData = data.toString();
-                            tv.setText("forecast for:");
+                            tv.setText(R.string.forecast_for +":"); // forecast for:
                             tvCity.setText(city+ ", " + state);
-//                                   weather + " " + temp_f + " " + humidity + " "+ wind +" "+ precipitation + "\n");
-//                                    + data.toString());
-//                            tv.setBackgroundColor(tv.getBackground()+10);
+
                             btn.setEnabled(true);
                             btn.setText(R.string.button_label2);
 
-                                // filter out insignificant data for better UX (ie: winds <2mph, humidity 0%)
+                                // bonus: filter out insignificant data for better UX (ie: winds <2mph, humidity 0%)
                                 String currentWeather = "Current weather:   "+ weather +" "+ String.valueOf(temp_f);
                                 if(!humidity.equals("0%")) currentWeather = currentWeather +"\n\t"+ humidity +" humid.\t";
                                 if(wind > 2) currentWeather = currentWeather +" "+ wind +" mph winds\t";
@@ -202,23 +217,24 @@ public class getZipCode extends AppCompatActivity {
                                 TextView tvCurrent = (TextView) findViewById(R.id.tvCurrentConditions);
                                 tvCurrent.setText(currentWeather);
 
-                                // Color-code by temperature
+                                // Color-code current weather by temperature
                                 if(temp_f>=60) tvCurrent.setBackgroundColor(ContextCompat.getColor(getBaseContext(),R.color.weather_warm));
                                     else tvCurrent.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.weather_cool));
 
-                                String[] arr = new String[10];
-                                arr[0] = "condition  temp.";
-                                arr[1] = currentWeather;
-                                arr[2] = "";
-                                arr[3] = "";
-                                arr[4] = "";
-                                arr[5] = "";
-                                arr[6] = "";
-                                arr[7] = "";
-                                arr[8] = "";
-                                arr[9] = "";
+
+                                String[] arr = new String[hourlyArr.length];
+                                int r=0;
+                                Log.d("arr[] rMax|length: ",String.valueOf(rMax) +"|"+ String.valueOf(hourlyArr.length));
+                                while (r<hourlyArr.length) {
+                                    arr[r] = hourlyArr[r][0] + " " + hourlyArr[r][1] + " " + hourlyArr[r][2];
+                                    Log.d("r",String.valueOf(r));
+                                    r++;
+                                }
+                                Log.d("status","arr[] filled. about to update listAdapter");
+
                                 listAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_list_item_1, arr); Log.d("listAdapter", arr[1]);
-                                lv.setAdapter(listAdapter); Log.d("lv", "adapter set");
+                            Log.d("status","listAdapter cread");
+                            lv.setAdapter(listAdapter); Log.d("lv", "adapter set");
 
                         }
                     });
