e5a72a915bbf17d3247a22b7aac8f087ac3e45b2
Bill Lugo <lugow1@mail.montclair.edu>
2017-09-28 14:31:20 -0400

    Splash screen (<1 sec.)

--- src/main/java/com/foo/umbrella/ui/MainActivity.java
+++ src/main/java/com/foo/umbrella/ui/MainActivity.java
@@ -1,9 +1,10 @@
 package com.foo.umbrella.ui;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.util.Log;
 
 import com.foo.umbrella.R;
 
@@ -14,27 +15,38 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
       //TODO: put in a background image to fill the Frame Layout with something attractive, yet simple
-      //TODO: Check network connection, toast a warning message if none
 
-      //TODO: load cached weater data from last time the app was used (if any), along with a datetime stamp
+      //TODO: load cached weather data from last time the app was used (if any), along with a datetime stamp
   }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        //    sleep((int) R.integer.splash_delay);  // prevents textView from displaying for some reason
-        int splashDelay = 1000;
-        TextView tv = (TextView) findViewById(R.id.textView3);
-//        sleep(splashDelay/3);
-//        tv.setText("3");
-//        sleep(splashDelay/3);
-//        tv.setText("2");
-//        sleep(splashDelay/3);
-//        tv.setText("1");
-//  sleep(0);
-        Intent i = new Intent(this,getZipCode.class);
-        startActivity(i);
+        try{new SplashTask().execute();} catch(Exception e){Log.d("Splash AsyncTask fail",e.toString());}
+    }
 
+    private class SplashTask extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+
+            // Delay
+            try{Thread.sleep(900);} catch(Exception e) {
+                Log.d("Splash delay error",e.toString());
+            }
+            //TODO: check network connection, Toast a warning if none
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+
+            try {
+                super.onPostExecute(o);
+                Intent i = new Intent(MainActivity.this, getZipCode.class);
+                startActivity(i);
+            } catch(Exception e) {Log.d("Splash onPostExecute",e.toString());}
+        }
     }
 }
